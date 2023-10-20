c7a9baa59deed68cd9a5c6587ebda4bd4f8a01e0
SolomiaSydoryk <sydoryk213@gmail.com>
2023-10-20 12:03:26 +0300

    added lab3

---
+++ src/main.py
@@ -0,0 +1,22 @@
+class BinaryTree:
+    def __init__(self, value, left=None, right=None, parent=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+
+def find_successor(tree: BinaryTree, node: BinaryTree) -> BinaryTree:
+    if node.right:
+        successor = node.right
+        while successor.left:
+            successor = successor.left
+        return successor
+    else:
+        parent = node.parent
+        while parent and parent.right == node:
+            node = parent
+            parent = parent.parent
+        return parent
+
+
---
+++ tests/test.py
@@ -0,0 +1,20 @@
+import unittest
+
+from src.main import find_successor, BinaryTree
+
+
+class TestFind(unittest.TestCase):
+    def test_first(self):
+        root = BinaryTree(10)
+        root.left = BinaryTree(5, parent=root)
+        root.right = BinaryTree(12, parent=root)
+        root.left.left = BinaryTree(3, parent=root.left)
+        root.left.right = BinaryTree(7, parent=root.left)
+        root.right.right = BinaryTree(20, parent=root.right)
+        root.right.right.left = BinaryTree(15, parent=root.right.right)
+        successor_node = find_successor(root, root.left.right)
+
+        self.assertEqual(successor_node.value, 10)
+
+    if __name__ == '__main__':
+        unittest.main()
