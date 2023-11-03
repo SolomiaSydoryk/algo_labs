af15c0f718d8e70a9d1b5f911c973077fdbce70b
SolomiaSydoryk <sydoryk213@gmail.com>
2023-11-03 11:48:36 +0200

    add lab4

--- src/main.py
+++ src/main.py
@@ -1,22 +1,47 @@
-class BinaryTree:
-    def __init__(self, value, left=None, right=None, parent=None):
-        self.value = value
-        self.left = left
-        self.right = right
-        self.parent = parent
+class Node:
+    def __init__(self, key):
+        self.value = key
+        self.left = None
+        self.right = None
 
 
-def find_successor(node: BinaryTree) -> BinaryTree:
-    if node.right:
-        successor = node.right
-        while successor.left:
-            successor = successor.left
-        return successor
-    else:
-        parent = node.parent
-        while parent and parent.right == node:
-            node = parent
-            parent = parent.parent
-        return parent
+def min_depth(root):
+    if root is None:
+        return 0
+    if root.left is None and root.right is None:
+        return 1
+    if root.left is None:
+        return min_depth(root.right) + 1
+    if root.right is None:
+        return min_depth(root.left) + 1
+    return min(min_depth(root.left), min_depth(root.right)) + 1
 
+
+def build_tree(edges):
+    node_list = {}
+    for edge in edges:
+        parent, child = map(int, edge.split(','))
+        if parent not in node_list:
+            node_list[parent] = Node(parent)
+        if child not in node_list:
+            node_list[child] = Node(child)
+        parent_node = node_list[parent]
+        child_node = node_list[child]
+        if parent_node.left is None:
+            parent_node.left = child_node
+        else:
+            parent_node.right = child_node
+    return node_list[1]
+
+
+if __name__ == "__main__":
+    with open('../input.txt', 'r') as input_file:
+        root_value = int(input_file.readline().strip())
+        edges = input_file.read().strip().split('\n')
+
+    root = build_tree(edges)
+    result = min_depth(root)
+
+    with open('../output.txt', 'w') as output_file:
+        output_file.write(str(result))
 
